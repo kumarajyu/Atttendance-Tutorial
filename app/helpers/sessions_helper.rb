ef1ed9eb594c4794ed54055e35335f84637255f0
kumarajyu <kumarajyu0@gmail.com>
2019-06-15 13:02:30 +0000

    Finish editing-users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,8 +18,24 @@ module SessionsHelper
     end
   end
   
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+  
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
     !current_user.nil?
+  end
+  
+  # 記憶しているURL (もしくはデフォルト値) にリダイレクトする
+  def redirect_back_or(default)
+    redirect_to(session[:fowarding_url] || default)
+    session.delete(:fowarding_url)
+  end
+  
+  # アクセスしようとしたURLを記憶しておく
+  def store_location
+    session[:fowarding_url] = request.original_url if request.get?
   end
 end
