1d8c7a2a0f9d4930c61efdcbfe74d48471ee4d95
kumarajyu <kumarajyu0@gmail.com>
2019-06-22 14:18:45 +0000

    Finish basic-information

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190616131735) do
+ActiveRecord::Schema.define(version: 20190617124945) do
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'boolern' for column 'admin'
